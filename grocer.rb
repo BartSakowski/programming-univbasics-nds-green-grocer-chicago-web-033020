f262c67b0d187ae07483308924026268d478f44c
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-20 20:10:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,7 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon = {
           :item => couponed_item_name,
           :price => coupons[i][:cost] / coupons[i][:num],
-          :count => coupons[i][:num]
+          :count => coupons[i][:num],
           :clearance => cart_item[:clearance]
         }
         cart << cart_item_with_coupon
